cb95ab62dd0f5a2b18026f244c9bbcf3f98b3d27
Gael-M0 <gmunoz33373@ucq.edu.mx>
2025-09-02 15:05:55 -0600

    0209

---
+++ udemy/python/Paquetes_Modulos_Propios/1.Modulos_Propios/UsoModulo.py
@@ -0,0 +1,11 @@
+import calculadora
+
+resultado_suma = calculadora.sumar(5, 3)
+resultado_resta = calculadora.restar(5, 3)
+resultado_multiplicacion = calculadora.multiplicar(5, 3)
+resultado_division = calculadora.dividir(5, 0)
+
+print("Suma:", resultado_suma)
+print("Resta:", resultado_resta)
+print("Multiplicación:", resultado_multiplicacion)
+print("División:", resultado_division)
---
+++ udemy/python/Paquetes_Modulos_Propios/2.Paquetes_Propios/2.PaquetesPropios.py
@@ -0,0 +1,8 @@
+from operaciones.sumar import sumar
+from operaciones.multiplicar import multiplicar
+
+resultado_suma = sumar(5, 3)
+resultado_multiplicacion = multiplicar(5, 3)
+
+print("Suma:", resultado_suma)
+print("Multiplicación:", resultado_multiplicacion)
