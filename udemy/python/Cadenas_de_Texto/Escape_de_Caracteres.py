cfe31d206b9003c6ce91f7941c582119cb3a579f
Gael-M0 <gmunoz33373@ucq.edu.mx>
2025-09-13 20:07:21 -0600

    1309

---
+++ udemy/python/Cadenas_de_Texto/Escape_de_Caracteres.py
@@ -0,0 +1,17 @@
+texto = "Hola \nMundo"
+print(texto)
+
+texto = "Hola \t\tMundo"
+print(texto)
+
+texto = "Hola \"Mundo\""
+print(texto)
+
+texto = "C:\\Usuarios"
+print(texto)
+
+texto = "Hola \rMundo"
+print(texto)
+
+texto = "Hola \bMundo"
+print(texto)
