8ce0561a9dc34b05b68c1bd89b4748b2126bfc5e
Gael-M0 <gmunoz33373@ucq.edu.mx>
2025-09-16 18:04:35 -0600

    1609

---
+++ udemy/python/Cadenas_de_Texto/MetodosCadenas.py
@@ -0,0 +1,36 @@
+cadeba = "Python123"
+
+metodo1 = cadeba.isalnum()
+print(metodo1)
+
+mdgocow = cadeba.isdigit()
+print(mdgocow)
+
+metodo2 = cadeba.isalpha()
+print(metodo2)
+
+palabras = ["hola", "Mundo", "Python"]
+conf = "-"
+
+metodo4 = conf.join(palabras)
+print(metodo4)
+
+cadena2 = "Hola Mundo Python"
+Metodo5 =cadena2.split()
+print(Metodo5)
+
+cadena3 = "Manzana,Banana,naranja"
+metodo6 = cadena3.split(",")
+print(metodo6)
+
+cadena4 = "Python"
+
+cadenaordenada = ''.join(sorted(cadena4))
+print(cadenaordenada)
+
+cadenaordenada = sorted(cadena4)
+print(cadenaordenada)
+
+cadena5 = "Python es un lenguaje de programacion"
+Metodo7 = cadena5.index("lenguaje")
+print(Metodo7)
---
+++ udemy/python/Cadenas_de_Texto/inNot.py
@@ -0,0 +1,12 @@
+cadena = "Python es un lenguaje de programacion"
+
+resultado1 = 'z' in cadena
+resultado2 = 'p' in cadena
+print(resultado1)
+print(resultado2)
+
+cadena1 = "Esta es la segunda cadena de texto"
+resultado3 = 'h' not in cadena1
+resultado4 = 'e' not in cadena1
+print(resultado3)
+print(resultado4)
